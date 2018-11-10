c2e27daf7592d862b68227cc5fe10bf0f2ff4ab0
Christopher Barton <bartcj2@student.op.ac.nz>
2018-11-11 09:29:46 +1300

    added dashboard and worked on navbar

--- studentDashboard.html.php
+++ studentDashboard.html.php
@@ -18,46 +18,77 @@
             $StudentListQuery = $pdo->query($selectString); 
             $sqll = "SELECT  * from sales_stats WHERE month='Mar' ";
 
-            $result = mysqli_query($conn, $sqll);
-            if (mysqli_num_rows($result) > 0) {
-                   // output data of each row
-                while ($row = mysqli_fetch_assoc($result)) {
-                    ?>
-            <div class="row">
-               <div class="col-xl-3 col-sm-6 mb-3">
-                  <div class="card text-white bg-primary o-hidden h-100">
-                     <div class="card-body">
-                        <div class="card-body-icon">
-                           <i class="fa fa-fw fa-comments"></i>
-                        </div>
-                        <div class="mr-5"><?php
-                                            echo $row['Vistors'];
-                                            ?> Vistors</div>
-                     </div>
-                     <a class="card-footer text-white clearfix small z-1" href="#">
-                     <span class="float-left">View Details</span>
-                     <span class="float-right">
-                     <i class="fa fa-angle-right"></i>
-                     </span>
-                     </a>
-                  </div>
-               </div>
-               <div class="col-xl-3 col-sm-6 mb-3">
-                  <div class="card text-white bg-warning o-hidden h-100">
-                     <div class="card-body">
-                        <div class="card-body-icon">
-                           <i class="fa fa-fw fa-list"></i>
-                        </div>
-                        <div class="mr-5"><?php
-                                            echo $row['revenue'];
-                                            ?>  Revenue</div>
-                     </div>
-                     <?php
+include 'connect.inc.php';
+$Uname = "2 Student";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $userMS, $passwordMS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec('SET NAMES "utf8"');
 
-                }
-            } else {
-                echo '0 results';
-            }
+    //echo "Connected<br/>"; // Debugging
+} catch (PDOException $e) {
+    $error = 'Connection to database failed';
+    include 'error.html.php';
+    exit();
+}
+
+
+try {
+    $selectString = 'SELECT lab.labname,xValue,yValue 
+	FROM data,lab
+  		WHERE data.labId=lab.labId
+      AND studentId= 1
+      AND data.toolId=1
+			';
+
+    $result = $conn->query($selectString);
+    /*
+        ---------------------------
+        example data: Table (googlechart)
+        --------------------------
+        weekly_task     percentage
+        Sleep           30
+        Watching Movie  10
+        job             40
+        Exercise        20
+     */
+
+    $rows = array();
+    $table = array();
+    $table['cols'] = array(
+
+        // Labels for your chart, these represent the column titles.
+        /*
+            note that one column is in "string" format and another one is in "number" format
+            as pie chart only required "numbers" for calculating percentage
+            and string will be used for Slice title
+     */
+        array('label' => 'labname', 'type' => 'string'),
+        array('label' => 'xValue', 'type' => 'number'),
+        array('label' => 'yValue', 'type' => 'number')
+    );
+    /* Extract the information from $result */
+    foreach ($result as $r) {
+        $temp = array();
+
+        // the following line will be used to slice the Pie chart
+
+        $temp[] = array('v' => (string)$r['labname']);
+        // Values of each slice
+
+        $temp[] = array('v' => (int)$r['xValue']);
+        $temp[] = array('v' => (int)$r['yValue']);
+        $rows[] = array('c' => $temp);
+    }
+
+    $table['rows'] = $rows;
+
+    // convert data into JSON format
+    $jsonTable = json_encode($table);
+    echo $jsonTable;
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
             ?>
                      <a class="card-footer text-white clearfix small z-1" href="#">
                      <span class="float-left">View Details</span>
